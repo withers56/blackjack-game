a57ba2dd3702a647794320811ef0a702dbfe3e3a
withers56 <william.k.withers@gmail.com>
2022-02-04 13:24:22 -0600

    added more game functions and started styling

--- js/game-functions.js
+++ js/game-functions.js
@@ -1,8 +1,8 @@
 'use strict';
+//Place your bet, then deal. player gets 2 cards, dealer gets 2, but one is down one is up.
+//you can hit, or stand. if you double, you double your bet and commit to only taking one more card
 
 
-let playerDeck = [];
-let pointValue = 0;
 
 let deck = [
     {suite: 'heart', value: '2'},
@@ -59,14 +59,107 @@ let deck = [
     {suite: 'spade', value: 'ace'},
 ];
 
+
+let playerDeck = [];
+let dealerDeck = [];
+let pointValue = 0;
 let num = 0;
+let moneyTotal = 1000;
+let bettingAmount = 0;
 let randomCard = {};
+let playersCards = $('#players-cards');
+let cardsPlayer = document.querySelector('#players-cards');
+let cardsDealer = document.querySelector('#dealers-cards');
+
 
 function hit () {
     num = Math.floor(Math.random() * 52);
     randomCard = {suite: deck[num].suite, value: deck[num].value};
     playerDeck.push(randomCard);
     console.log(`You pulled a ${randomCard.value} of ${randomCard.suite}s`)
+    displayPlayerCards();
+    addingScore();
+    if (pointValue === 21) {
+        console.log('Blackjack!');
+        resetDecks();
+        pointValue = 0;
+        $('.money-score').html('Cards value: ' + pointValue)
+    }
+    if (pointValue > 21) {
+        console.log('loser');
+        resetDecks();
+        pointValue = 0;
+        $('.money-score').html('Cards value: ' + pointValue)
+    }
+}
+
+function displayPlayerCards () {
+    let suite = randomCard.suite;
+    let value = randomCard.value;
+
+    if (value === 'jack')
+        value = 'J';
+    if (value === 'queen')
+        value = 'Q';
+    if (value === 'king')
+        value = 'K';
+    if (value === 'ace')
+        value = 'A'
+
+    if (suite === 'heart') {
+        cardsPlayer.innerHTML += '<div class="card"><span class="suite pl-1"><i class="bi bi-suit-heart-fill" style="color: red"></i></span><span class="card-num pt-0 mt-0" style="color: red">' + value + '</span></div>'
+    }
+    if (suite === 'diamond') {
+        cardsPlayer.innerHTML += '<div class="card"><span class="suite pl-1"><i class="bi bi-suit-diamond-fill" style="color: red"></i></span><span class="card-num pt-0 mt-0" style="color: red">' + value + '</span></div>'
+    }
+    if (suite === 'spade') {
+        cardsPlayer.innerHTML += '<div class="card"><span class="suite pl-1"><i class="bi bi-suit-spade-fill"></i></span><span class="card-num pt-0 mt-0">' + value + '</span></div>'
+    }
+    if (suite === 'club') {
+        cardsPlayer.innerHTML += '<div class="card"><span class="suite pl-1"><i class="bi bi-suit-club-fill"></i></span><span class="card-num pt-0 mt-0">' + value + '</span></div>'
+    }
+
+}
+
+function dealersHand () {
+    num = Math.floor(Math.random() * 52);
+    randomCard = {suite: deck[num].suite, value: deck[num].value};
+
+    dealerDeck.push(randomCard);
+    displayDealerCards();
+
+}
+function dealerSetup () {
+    dealersHand();
+    dealersHand();
+}
+
+function displayDealerCards () {
+    let suite = randomCard.suite;
+    let value = randomCard.value;
+
+    if (value === 'jack')
+        value = 'J';
+    if (value === 'queen')
+        value = 'Q';
+    if (value === 'king')
+        value = 'K';
+    if (value === 'ace')
+        value = 'A'
+
+    if (suite === 'heart') {
+        cardsDealer.innerHTML += '<div class="card"><span class="suite pl-1"><i class="bi bi-suit-heart-fill" style="color: red"></i></span><span class="card-num pt-0 mt-0" style="color: red">' + value + '</span></div>'
+    }
+    if (suite === 'diamond') {
+        cardsDealer.innerHTML += '<div class="card"><span class="suite pl-1"><i class="bi bi-suit-diamond-fill" style="color: red"></i></span><span class="card-num pt-0 mt-0" style="color: red">' + value + '</span></div>'
+    }
+    if (suite === 'spade') {
+        cardsDealer.innerHTML += '<div class="card"><span class="suite pl-1"><i class="bi bi-suit-spade-fill"></i></span><span class="card-num pt-0 mt-0">' + value + '</span></div>'
+    }
+    if (suite === 'club') {
+        cardsDealer.innerHTML += '<div class="card"><span class="suite pl-1"><i class="bi bi-suit-club-fill"></i></span><span class="card-num pt-0 mt-0">' + value + '</span></div>'
+    }
+
 }
 
 // hit(playerDeck, randomCard);
@@ -91,28 +184,66 @@ function addingScore(){
             pointValue += parseInt(playerDeck[i].value);
         }
     }
+    $('.money-score').html('Cards value: ' + pointValue)
 }
 
 function inPlay() {
+    dealersHand();
+    dealersHand();
     hit();
     addingScore();
+    hit();
+    addingScore();
+
     console.log(`Your score is: ${pointValue}`);
     if (pointValue === 21) {
         console.log('Blackjack!');
-        resetPlayerDeck();
+        resetDecks();
+        pointValue = 0;
+
     }
     if (pointValue > 21) {
         console.log('loser');
-        resetPlayerDeck();
+        resetDecks();
+        pointValue = 0;
     }
+    // location.reload();
 }
 
-function resetPlayerDeck () {
+function resetDecks () {
     playerDeck = [];
+    dealerDeck = [];
+    cardsPlayer.innerHTML = '';
+    cardsDealer.innerHTML = '';
 }
 
 
 
 // console.log(playerDeck);
+// startup()
+// inPlay()
+$('#hit-btn').click(hit);
 
-$('#hit-btn').click(inPlay);
+function startup() {
+    let isPlaying = confirm("Would you like to play?");
+
+    if (isPlaying) {
+        console.log('yeet');
+    }
+
+
+}
+
+function betting (e) {
+    let bettingNumber = parseInt( $(this).text());
+    bettingAmount += bettingNumber;
+    console.log(bettingAmount);
+}
+
+$('#1').click(betting);
+$('#5').click(betting);
+$('#25').click(betting);
+$('#50').click(betting);
+$('#100').click(betting);
+
+$('#bet').click(inPlay)
